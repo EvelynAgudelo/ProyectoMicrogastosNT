b2a13bf338572e87b61dac73d72c80f9bf667648
Evelyn <evelinagudelosepulveda@gmail.com>
2026-05-29 07:40:10 -0500

    Acctulizado

--- AnalisisMicroGastos.py
+++ AnalisisMicroGastos.py
@@ -3,12 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # URL de la API
-url = "http://localhost:8080/microgastosapp/v1/gastos/usuario/3"
+url = "http://localhost:8080/microgastosapp/v1/gastos/usuario/7"
 
-# Consumir la API
+# Consumir la API: Este muestra los datos recibidos desde la API
 response = requests.get(url)
+print("Código:", response.status_code)
+print("Respuesta:", response.text)
 
-# Convertir JSON
+# Convertir JSON: Transforma la respuesta de la API en una estructura que Python puede manipular
 gastos = response.json()
 
 # Lista para almacenar datos
@@ -40,15 +42,7 @@ plt.ylabel("Valor")
 plt.tight_layout()
 plt.show()
 
-# GRÁFICA 2 - Distribución porcentual de gastos por descripción
-gastos_descripcion.plot(kind="pie", autopct="%1.1f%%")
-
-plt.title("Distribución de Gastos")
-plt.ylabel("")
-
-plt.show()
-
-# GRÁFICA 3 - Gastos por fecha
+# GRÁFICA 2 - Gastos por fecha
 gastos_fecha = df.groupby("fecha")["valor"].sum()
 
 gastos_fecha.plot(kind="line", marker="o")
